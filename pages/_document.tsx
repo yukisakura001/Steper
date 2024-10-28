d73152522189b783a7e5501e0e56e3afab750926
natuyukisakura <MoriRinMonro1@gmail.com>
2024-10-28 23:00:27 +0900

    (update)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,6 +4,9 @@ export default function Document() {
   return (
     <Html lang="ja">
       <Head />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon.png"></link>
+      <meta name="theme-color" content="#b8e986" />
       <body className="antialiased">
         <Main />
         <NextScript />
