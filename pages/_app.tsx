b5dfc800b64c54e8ed207b717d82215eb951084e
natuyukisakura <MoriRinMonro1@gmail.com>
2024-11-10 11:42:19 +0900

    [update] ogp

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,10 +3,27 @@ import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import { AuthProvider } from "@/context/auth";
 import FootBar from "@/components/footBar";
+import Head from "next/head";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <AuthProvider>
+      <Head>
+        <meta property="og:title" content="Steper" />
+        <meta
+          property="og:description"
+          content="目標達成を応援するwebアプリです。"
+        />
+        <meta property="og:image" content="" />
+        <meta property="og:url" content="../public/icon512_rounded.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:title" content="Steper" />
+        <meta
+          name="twitter:description"
+          content="目標達成を応援するwebアプリです。"
+        />
+        <meta name="twitter:image" content="icon512_rounded" />
+      </Head>
       <div>
         <Navbar />
         <Component {...pageProps} />
