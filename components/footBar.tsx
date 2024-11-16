6bed395e6e2df1718422dddfb46c88d0c6d21071
natuyukisakura <MoriRinMonro1@gmail.com>
2024-11-16 21:47:06 +0900

    (fix)

--- components/footBar.tsx
+++ components/footBar.tsx
@@ -7,7 +7,7 @@ export default function FootBar() {
     <footer className="text-gray-600 body-font">
       <div className="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
         <p className="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">
-          © 2024 Sterper
+          © 2024 Steper
         </p>
         <span className="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
           <a href="https://github.com/yukisakura001/Steper">
